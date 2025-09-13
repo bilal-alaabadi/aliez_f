bff82d6fc880f404f456a3f811e509fe568b9199
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-09-13 15:58:45 +0400

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,5 +2,5 @@
 //     return 'https://collection-b.vercel.app';//رابط الباك اند بدل 5000
 // }
 export const getBaseUrl = () => {
-    return 'http://localhost:5000';//رابط الباك اند بدل 5000 رجع
+    return 'https://aliezb.vercel.app';//رابط الباك اند بدل 5000 رجع
 } 
